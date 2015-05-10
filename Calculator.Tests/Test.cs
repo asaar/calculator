48f7a5bb70f7eb27709f7c91b1c6e5841458c450
Tobias Schulz <tobias.schulz186@gmail.com>
2015-05-10 18:16:03 +0200

    Initial commit

---
+++ Calculator.Tests/Test.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Calculator.Tests
+{
+	[TestFixture ()]
+	public class Test
+	{
+		Calculator calc = new Calculator ();
+
+		[TestFixtureSetUp ()]
+		public void Setup ()
+		{
+			Core.Common.Logging.Enable ();
+		}
+
+		[Test ()]
+		public void TestCase ()
+		{
+			Assert.AreEqual (5, (calc.Eval (exprString: "2+3") as NumericOperand)?.Value);
+			Assert.AreEqual (10, (calc.Eval (exprString: "2*3+4") as NumericOperand)?.Value);
+			Assert.AreEqual (11, (calc.Eval (exprString: "2*3+(4+1)") as NumericOperand)?.Value);
+			Assert.AreEqual (10, (calc.Eval (exprString: "2+3+(4+1)") as NumericOperand)?.Value);
+			Assert.AreEqual (10, (calc.Eval (exprString: "2+2*(1+3)") as NumericOperand)?.Value);
+
+			Assert.AreEqual (8, (calc.Eval (exprString: "2^3") as NumericOperand)?.Value);
+			Assert.AreEqual (5, (calc.Eval (exprString: "2^3 - 3") as NumericOperand)?.Value);
+			Assert.AreEqual (6, (calc.Eval (exprString: "2^3 - 3 + 1") as NumericOperand)?.Value);
+			Assert.AreEqual (6, (calc.Eval (exprString: "3 * 10 / 5 ") as NumericOperand)?.Value);
+			Assert.AreEqual (12, (calc.Eval (exprString: "(2^3 - 3 + 1) + (3 * 10 / 5)") as NumericOperand)?.Value);
+			Assert.AreEqual (12, (calc.Eval (exprString: "2^3 - 3 + 1 + 3 * 10 / 5 ") as NumericOperand)?.Value);
+
+			Assert.AreEqual (7, (calc.Eval (exprString: "2^3 - 3 + 1 + 3 * 10 / 5 - 5") as NumericOperand)?.Value);
+
+			Assert.AreEqual (7, (calc.Eval (exprString: "2^3 - 3 + 1 + 3 * ((4+4*4)/2) / 5 + -5") as NumericOperand)?.Value);
+		}
+
+		[TestFixtureTearDown ()]
+		public void TearDown ()
+		{
+			Core.Common.Logging.Finish ();
+		}
+	}
+}
+
---
+++ Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calculator
+{
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Core.Common.Logging.Enable ();
+
+			Console.WriteLine ("Hello World!");
+			Calculator calc = new Calculator ();
+
+			//calc.Eval (exprString: "2+3");
+
+			//calc.Eval (exprString: "2*3+4");
+			//calc.Eval (exprString: "2*3+(4+1)");
+			//calc.Eval (exprString: "2+3+(4+1)");
+			//calc.Eval (exprString: "2+3*(2)");
+			calc.Eval (exprString: "2+2*(1+3)");
+
+			//calc.Eval (exprString: "10  +(-2*3*(-1))");
+			// "2^3 - 3 + 1 + 3 * ((4+4*4)/2) / 5 + -5"
+
+			Core.Common.Logging.Finish ();
+		}
+	}
+}
